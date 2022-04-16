4df30a585e77d165a931d7b070cb5f22b217e683
relaxyg <relaxyg4@gmail.com>
2022-04-16 23:06:20 +0600

    configure photo upload

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -57,6 +57,6 @@ class ItemsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def item_params
-      params.require(:item).permit(:name, :description, :price, :address, :amount)
+      params.require(:item).permit(:name, :description, :price, :address, :amount, :photo)
     end
 end
