79944cacd0fca420fc0b03bf736e59b60d3fd5c2
relaxyg <relaxyg4@gmail.com>
2022-04-16 21:44:35 +0600

    add bootstrap

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,6 @@
 class ReviewsController < ApplicationController
+  def new
+    @item = Item.find(params[:item_id])
+    @review = Review.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :items
   devise_for :users
+  resources :items do
+    resources :reviews, only: [:new, :create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'items#index'
 end
